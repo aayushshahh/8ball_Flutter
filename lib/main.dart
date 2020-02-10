8ad536f4c5812bd304038aff7b229c3e77cd4aa5
aayushshahh <53306347+aayushshahh@users.noreply.github.com>
2020-02-10 21:30:55 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          appBar: AppBar(
+            title: Text('Magic 8 Ball'),
+            backgroundColor: Colors.teal[400],
+          ),
+          body: ballApp(),
+        ),
       ),
     );
+
+class ballApp extends StatefulWidget {
+  @override
+  _ballAppState createState() => _ballAppState();
+}
+
+class _ballAppState extends State<ballApp> {
+  int num = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  num = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$num.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
